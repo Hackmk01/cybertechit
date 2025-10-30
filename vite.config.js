108af27de18d78e2025e82da416951dabe8f8248
Jorge Hackmk <jorge@cybertechit.cl>
2025-10-30 18:48:04 -0300

    fix vite config & remove nodemailer

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',                 // permite acceso desde otros dispositivos
+    host: true,
     port: 5173,
-    strictPort: true,
-    allowedHosts: ['cybertech01', 'localhost'] // agrega tu host si quieres
+  },
+  preview: {
+    port: 4173
+  },
+  build: {
+    outDir: "dist"
   }
 })
